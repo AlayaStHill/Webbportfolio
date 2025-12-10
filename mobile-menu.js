dbf3b9be45202033279e930426b90f4e0f15b1c8
AlayaStHill <alaya.st.hill@gmail.com>
2025-12-10 22:13:33 +0100

    Add mobile view burger menu. Add mobile view navbar and JS toggle function.

---
+++ mobile-menu.js
@@ -0,0 +1,22 @@
+const menuBurgerBtn = document.getElementById("menu-burger-btn");
+
+const mobileNav = document.getElementById("mobile-nav");
+
+
+function toggleMenu() {
+    if (mobileNav.classList.contains("show-menu")) {
+
+        mobileNav.classList.remove("show-menu");
+    } else {
+        mobileNav.classList.add("show-menu");
+    }
+}
+
+menuBurgerBtn.addEventListener("click", toggleMenu);
+
+const mobileListItems = document.querySelectorAll(".mobile-list-item");
+
+mobileListItems.forEach(element => {
+
+    element.addEventListener("click", toggleMenu);
+});
